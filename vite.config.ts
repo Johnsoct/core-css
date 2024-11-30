234cc17a9ff317fd75873d4cbb8a3582742dfcc5
Chris Johnson <johnsoctbus@gmail.com>
2024-11-30 11:35:28 -0500

    WIP build

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+// Packages
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+        build: {
+                emptyOutDir: false,
+
+                lib: {
+                        entry: path.resolve(__dirname, 'src/main.ts'),
+                        fileName: "index",
+                        name: 'core-css',
+                },
+
+                outDir: "../dist",
+
+                minify: true,
+
+                rollupOptions: {
+                        // Prevent vue from being bundled with the final build
+                        external: [ 'vite' ],
+                },
+        },
+
+        css: {
+                preprocessorOptions: {
+                        scss: {
+                                api: 'modern-compiler',
+                        },
+                },
+        },
+
+        resolve: {
+                alias: {
+                        '@styles': path.resolve(__dirname, './styles'),
+                },
+        },
+
+        root: 'src',
+});
