f726343de812c841dcd63f289a66d95e21f04315
Chris Johnson <johnsoctbus@gmail.com>
2025-02-13 04:50:44 -0500

    set indentation to 4

--- vite.config.ts
+++ vite.config.ts
@@ -4,38 +4,38 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-        build: {
-                emptyOutDir: false,
+    build: {
+        emptyOutDir: false,
 
-                lib: {
-                        entry: path.resolve(__dirname, 'src/main.ts'),
-                        fileName: "index",
-                        name: 'core-css',
-                },
-
-                outDir: "../dist",
-
-                minify: true,
-
-                rollupOptions: {
-                        // Prevent vue from being bundled with the final build
-                        external: [ 'vite' ],
-                },
+        lib: {
+            entry: path.resolve(__dirname, 'src/main.ts'),
+            fileName: "index",
+            name: 'core-css',
         },
 
-        css: {
-                preprocessorOptions: {
-                        scss: {
-                                api: 'modern-compiler',
-                        },
-                },
-        },
+        outDir: "../dist",
 
-        resolve: {
-                alias: {
-                        '@styles': path.resolve(__dirname, './styles'),
-                },
-        },
+        minify: true,
 
-        root: 'src',
+        rollupOptions: {
+            // Prevent vue from being bundled with the final build
+            external: [ 'vite' ],
+        },
+    },
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            },
+        },
+    },
+
+    resolve: {
+        alias: {
+            '@styles': path.resolve(__dirname, './styles'),
+        },
+    },
+
+    root: 'src',
 });
