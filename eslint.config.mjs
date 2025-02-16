00ee83921df6db1e7f46393f12b1a87804da9c9d
Chris Johnson <johnsoctbus@gmail.com>
2025-02-16 09:33:13 -0500

    added html-eslint to enforce the style of HTML most readable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,35 @@
 import globals from "globals";
+import html from "@html-eslint/eslint-plugin";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const htmlRules = {
+    ...html.configs["flat/recommended"].rules,
+    "@html-eslint/attrs-newline": ["error", {
+        "closeStyle": "newline",
+        "ifAttrsMoreThan": 1,
+    }],
+}
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ["src/**/*.{js,mjs,cjs,ts}"] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+    // HTML
+    { 
+        ...html.configs["flat/recommended"], 
+        files: ["src/**/*.html"], 
+        rules: htmlRules,
+    },
+
+    // JS/TS
+    { 
+        ...pluginJs.configs.recommended,
+        ...tseslint.configs.recommended,
+        files: ["src/**/*.{js,mjs,cjs,ts}"]
+    },
+    
+    {
+        languageOptions: {
+            globals: globals.browser,
+        }
+    },
 ];
